3988ba3273be9a24080569d9a402e7e6dbdc553f
Luciana Mariano Alici <marianoaliciluciana@gmail.com>
2025-09-17 17:57:22 -0300

    Cambio comillas por #

--- src/scoring.py
+++ src/scoring.py
@@ -1,1 +1,19 @@
+from typing import Dict
 
+def puntaje_equipo(param):
+    
+# Calculo el puntaje de cada equipo en cada ronda:
+# +3 puntos por cada punto de innovación.
+# +1 punto por cada punto de presentación.
+# -1 punto si tuvo errores graves (True).
+
+    # variable auxiliar para innovación
+    puntos_innovacion = 3 * param['innovacion']
+    # variable auxiliar para presentación
+    puntos_presentacion = param['presentacion']
+    # variable auxiliar para errores (1 si hay error, 0 si no)
+    penalizacion_error = 1 if param['errores'] else 0
+    # suma final
+    puntaje_total = puntos_innovacion + puntos_presentacion - penalizacion_error
+
+    return puntaje_total
